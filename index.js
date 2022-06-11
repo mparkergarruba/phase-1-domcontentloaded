13ebfeba628cb4f4cc8a582df8f26a89ef88d5f1
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-11 14:37:27 -0400

    commit tree ad22191f1fbf8321c897fc7a2073e322d575d4fd

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textGrab = document.querySelector("#text")
+    textGrab.textContent = "This is really cool!"
+})
